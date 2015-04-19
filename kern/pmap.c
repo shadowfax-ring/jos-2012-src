600b83bf00c8318dea68865b2bee2cb237db7020
Ruocheng Li <lrc.bill@gmail.com>
2015-04-18 23:50:49 -0700

    Finished Lab 4 Exercise 3 - passed paging checks

--- kern/pmap.c
+++ kern/pmap.c
@@ -239,8 +239,8 @@ mem_init(void)
 			PDX(KSTACKTOP-KSTKSIZE));
 	cprintf(MAG_FG "Map bootstack\n" RST);
 #endif
-	boot_map_region(kern_pgdir, KSTACKTOP - KSTKSIZE, KSTKSIZE, 
-					PADDR(bootstack), PTE_W | PTE_P);
+	//boot_map_region(kern_pgdir, KSTACKTOP - KSTKSIZE, KSTKSIZE, 
+					//PADDR(bootstack), PTE_W | PTE_P);
 
 	//////////////////////////////////////////////////////////////////////
 	// Map all of physical memory at KERNBASE.
@@ -341,7 +341,15 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+	int i;
+	for (i = 0; i < NCPU; i++) {
+		uintptr_t kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		boot_map_region(kern_pgdir,
+						kstacktop_i - KSTKSIZE,
+						KSTKSIZE,
+						PADDR(percpu_kstacks[i]),
+						PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
