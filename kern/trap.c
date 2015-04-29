59eaf747a244f2853fad90dc182c3d2ed17d3d12
Ruocheng Li <lrc.bill@gmail.com>
2015-04-28 21:58:01 -0700

    Finished Lab 4 Exercise 9

--- kern/trap.c
+++ kern/trap.c
@@ -337,11 +337,38 @@ page_fault_handler(struct Trapframe *tf)
 	//   (the 'tf' variable points at 'curenv->env_tf').
 
 	// LAB 4: Your code here.
-
+	// If page fault upcall does not exist
 	// Destroy the environment that caused the fault.
-	cprintf("[%08x] user fault va %08x ip %08x\n",
-		curenv->env_id, fault_va, tf->tf_eip);
-	print_trapframe(tf);
-	env_destroy(curenv);
+	if (!curenv->env_pgfault_upcall) {
+		cprintf("[%08x] user fault va %08x ip %08x\n",
+			curenv->env_id, fault_va, tf->tf_eip);
+		print_trapframe(tf);
+		env_destroy(curenv);
+	}
+
+	struct UTrapframe *utf;
+
+	// recursive page fault upcall
+	if (tf->tf_esp >= UXSTACKTOP - PGSIZE && tf->tf_esp < UXSTACKTOP) {
+		utf = (struct UTrapframe *) (tf->tf_esp - sizeof(struct UTrapframe) - 4);
+	} else {
+		utf = (struct UTrapframe *) (UXSTACKTOP - sizeof(struct UTrapframe));
+	}
+
+	// check memory mapping for user exception stack
+	user_mem_assert(curenv, (void *) utf, sizeof(struct UTrapframe), PTE_U|PTE_W);
+
+	// set up trapframe on user exception stack
+	utf->utf_fault_va = fault_va;
+	utf->utf_err = tf->tf_err;
+	utf->utf_regs = tf->tf_regs;
+	utf->utf_eip = tf->tf_eip;
+	utf->utf_eflags = tf->tf_eflags;
+	utf->utf_esp = tf->tf_esp;
+
+	// start page fault upcall in user exception stack
+	curenv->env_tf.tf_eip = (uintptr_t) curenv->env_pgfault_upcall;
+	curenv->env_tf.tf_esp = (uintptr_t) utf;
+	env_run(curenv);
 }
 
