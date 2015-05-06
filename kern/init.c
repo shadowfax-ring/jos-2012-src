dd545bdad0e02fe35b5def10af5e8bf5972cb3a5
Ruocheng Li <lrc.bill@gmail.com>
2015-05-05 23:53:14 -0700

    Lab 4 Exercise 12 Copy-on-Write in progress

--- kern/init.c
+++ kern/init.c
@@ -98,7 +98,7 @@ i386_init(void)
 #else
 	// Touch all you want.
 	//ENV_CREATE(user_primes, ENV_TYPE_USER);
-	ENV_CREATE(user_faultdie, ENV_TYPE_USER);
+	ENV_CREATE(user_forktree, ENV_TYPE_USER);
 #endif // TEST*
 
 	// Schedule and run the first user environment!
