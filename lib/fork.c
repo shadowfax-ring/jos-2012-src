dd390c7c16e2cbcec4f48e309cc962713ab820cd
Ruocheng Li <lrc.bill@gmail.com>
2015-05-06 22:38:40 -0700

    Completed Lab 4 Exercise 12 yet not pass test

--- lib/fork.c
+++ lib/fork.c
@@ -23,8 +23,10 @@ pgfault(struct UTrapframe *utf)
 	// Hint:
 	//   Use the read-only page table mappings at uvpt
 	//   (see <inc/memlayout.h>).
-	if (!(err & FEC_WR) || !(uvpd[PDX(addr)] & PTE_P) ||
-		!(uvpt[PGNUM(addr)] & PTE_COW)) {
+	pte_t pte = uvpt[PGNUM(addr)];
+	if (!((err & FEC_WR) && (uvpd[PDX(addr)] & PTE_P) &&
+		(pte & PTE_COW))) {
+		cprintf("%d %d %d\n", err & FEC_WR, uvpd[PDX(addr)] & PTE_P, pte & PTE_COW);
 		panic("Page fault access is not a write or to a COW page.");
 	}
 
@@ -128,8 +130,30 @@ fork(void)
     }
 
 	// if it's a parent
+	uintptr_t addr;
 
+	for (addr = UTEXT; addr <= UTOP - PGSIZE; addr += PGSIZE) {
+		int pn = PGNUM(addr);
+		pte_t pte = uvpt[pn];
+		if ((pte & PTE_P) || (pte & PTE_U)) {
+			if (duppage(envid, pn) < 0) {
+				panic("duppage: map error.");
+			}
+		}
+	}
 
+	if (sys_page_alloc(envid, (void *) (UXSTACKTOP - PGSIZE), PTE_P|PTE_U|PTE_W) < 0) {
+		panic("sys_page_alloc: failed to allocate page for user exception stack.");
+	}
+
+	extern void _pgfault_upcall(void);
+	if (sys_env_set_pgfault_upcall(envid, _pgfault_upcall) < 0) {
+		panic("sys_env_set_pgfault_upcall: failed to set pgfault upcall in child.");
+	}
+
+	if (sys_env_set_status(envid, ENV_RUNNABLE) < 0) {
+		panic("sys_env_set_status: failed to set status for envid.");
+	}
 
 	return envid;
 }
